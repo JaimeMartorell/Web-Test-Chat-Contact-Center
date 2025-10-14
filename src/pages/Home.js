2a9f5f784f5704bf1842b77515bafe896c23c4fd
JaimeMartorell <165386376+JaimeMartorell@users.noreply.github.com>
2025-10-14 18:06:55 +0200

    Update Home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,10 @@
-
+import ZoomChatEmbed from '../components/ZoomChatEmbed';
 const Home = () => {
   return (
     <div>
       <h1>Home Page</h1>
       <button id="zoom-chat-launch-button">Iniciar Chat</button>
+    <ZoomChatEmbed />
  
     </div>
   );
