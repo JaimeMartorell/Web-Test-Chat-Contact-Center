1ec17f1cfac7cd4d70b42774ae1d52d57a034e6d
JaimeMartorell <165386376+JaimeMartorell@users.noreply.github.com>
2025-10-14 17:13:16 +0200

    Update ZoomChatEmbed.js

--- src/components/ZoomChatEmbed.js
+++ src/components/ZoomChatEmbed.js
@@ -3,7 +3,7 @@ import { useEffect } from 'react';
 const ZoomChatEmbed = () => {
   useEffect(() => {
     const script = document.createElement('script');
-    script.setAttribute('data-apikey', 'TU_API_KEY');
+    script.setAttribute('data-apikey', 't6_4K8IvQ3qa-DvONwEiaA');
     script.src = 'https://us01ccistatic.zoom.us/us01cci/web-sdk/chat-client.js';
     document.body.appendChild(script);
 
