1af1deaf6e1dc9111508732ec92dbcb9ff54c6d3
JaimeMartorell <165386376+JaimeMartorell@users.noreply.github.com>
2025-10-16 11:15:44 +0200

    Refactor chat button and embed integration

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,14 +1,40 @@
 import ZoomChatEmbed from '../components/ZoomChatEmbed';
+
 const Home = () => {
+  const handleChatClick = () => {
+    const accionBtn = document.getElementById('ACCION');
+    if (accionBtn) {
+      accionBtn.click();
+    }
+  };
+
   return (
     <div>
       <h1>Home Page</h1>
-      <button id="zoom-chat-launch-button">Iniciar Chat</button>
-    <ZoomChatEmbed />
- 
+
+      <button
+        type="button"
+        id="BOTON"
+        name="BOTON"
+        className="mr-2 btn-primary btn w-20"
+        onClick={handleChatClick}
+      >
+        Iniciar Chat
+      </button>
+
+      <button
+        type="submit"
+        id="ACCION"
+        name="ACCION"
+        className="d-none btn-outline-primary mr-2 btn"
+      >
+        ACCION
+        <div style={{ display: 'none' }}></div>
+      </button>
+
+      <ZoomChatEmbed />
     </div>
   );
 };
 
 export default Home;
-
